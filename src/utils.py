bd66df4e2d6bf94f51a394e1aaf5f1c2dee5f00f
Sven Thiele <sthiele78@gmail.com>
2014-05-21 14:26:31 +0200

    use pyasp-1.3.3

--- src/utils.py
+++ src/utils.py
@@ -30,3 +30,4 @@ def print_met(predictions) :
   for p in predictions: 
     if p.pred() == "xreaction" : print '  '+str(p.arg(0))
     if p.pred() == "unproducible_target" : print '  '+str(p.arg(0))
+    if p.pred() == "target" : print '  '+str(p.arg(0))
