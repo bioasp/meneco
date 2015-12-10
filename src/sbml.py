36e5dce25ffb36998987d49267f9647ce620ec1e
Sven Thiele <sthiele78@gmail.com>
2015-12-10 09:19:26 +0100

    cleanup

--- src/sbml.py
+++ src/sbml.py
@@ -24,85 +24,84 @@ from xml.etree.ElementTree import XML, fromstring, tostring
 
 
 def get_model(sbml):
-    model_element = None
-    for e in sbml:
-        if e.tag[0] == "{":
-          uri, tag = e.tag[1:].split("}")
-        else: tag = e.tag
-        if tag == "model":
-          model_element = e
-          break
-    return model_element
+  model_element = None
+  for e in sbml:
+    if e.tag[0] == "{":
+      uri, tag = e.tag[1:].split("}")
+    else: tag = e.tag
+    if tag == "model":
+      model_element = e
+      break
+  turn model_element
 
 def get_listOfSpecies(model):
-    listOfSpecies = None
-    for e in model:
-        if e.tag[0] == "{":
-          uri, tag = e.tag[1:].split("}")
-        else: tag = e.tag
-        if tag == "listOfSpecies":
-          listOfSpecies = e
-          break
-    return listOfSpecies
+  listOfSpecies = None
+  for e in model:
+    if e.tag[0] == "{":
+      uri, tag = e.tag[1:].split("}")
+    else: tag = e.tag
+    if tag == "listOfSpecies":
+      listOfSpecies = e
+      break
+  return listOfSpecies
 
 def get_listOfReactions(model):
-    listOfReactions = None
-    for e in model:
-        if e.tag[0] == "{":
-          uri, tag = e.tag[1:].split("}")
-        else: tag = e.tag
-        if tag == "listOfReactions":
-          listOfReactions = e
-          break
-    return listOfReactions
+  listOfReactions = None
+  for e in model:
+    if e.tag[0] == "{":
+      uri, tag = e.tag[1:].split("}")
+    else: tag = e.tag
+    if tag == "listOfReactions":
+      listOfReactions = e
+      break
+  return listOfReactions
 
 def get_listOfReactants(reaction):
-    listOfReactants = None
-    for e in reaction:
-        if e.tag[0] == "{":
-          uri, tag = e.tag[1:].split("}")
-        else: tag = e.tag
-        if tag == "listOfReactants":
-          listOfReactants = e
-          break
-    return listOfReactants
+  listOfReactants = None
+  for e in reaction:
+    if e.tag[0] == "{":
+      uri, tag = e.tag[1:].split("}")
+    else: tag = e.tag
+    if tag == "listOfReactants":
+      listOfReactants = e
+      break
+  return listOfReactants
 
 def get_listOfProducts(reaction):
-    listOfProducts = None
-    for e in reaction:
-        if e.tag[0] == "{":
-          uri, tag = e.tag[1:].split("}")
-        else: tag = e.tag
-        if tag == "listOfProducts":
-          listOfProducts = e
-          break
-    return listOfProducts
+  listOfProducts = None
+  for e in reaction:
+    if e.tag[0] == "{":
+      uri, tag = e.tag[1:].split("}")
+    else: tag = e.tag
+    if tag == "listOfProducts":
+      listOfProducts = e
+      break
+  return listOfProducts
 
 def get_score(reaction):
-    score = 0
-    for e in reaction:
-      if e.tag[0] == "{":
-        uri, tag = e.tag[1:].split("}")
-      else: tag = e.tag
-      if tag == "annotation":
-        for s in e:
-          if s.tag[0] == "{":
-            suri, stag = s.tag[1:].split("}")
-          else: stag = s.tag
-          if stag == "score":
-            score = s.attrib.get("val")
-    return score
+  score = 0
+  for e in reaction:
+    if e.tag[0] == "{":
+      uri, tag = e.tag[1:].split("}")
+    else: tag = e.tag
+    if tag == "annotation":
+      for s in e:
+        if s.tag[0] == "{":
+          suri, stag = s.tag[1:].split("}")
+        else: stag = s.tag
+        if stag == "score":
+          score = s.attrib.get("val")
+  return score
 
 
 def readSBMLnetwork(filename, name) :
 
-  lpfacts = TermSet()
-
-  tree = etree.parse(filename)
-  sbml = tree.getroot()
-  model = get_model(sbml)
-
+  lpfacts         = TermSet()
+  tree            = etree.parse(filename)
+  sbml            = tree.getroot()
+  model           = get_model(sbml)
   listOfReactions = get_listOfReactions(model)
+
   for e in listOfReactions:
     if e.tag[0] == "{":
       uri, tag = e.tag[1:].split("}")
@@ -115,27 +114,26 @@ def readSBMLnetwork(filename, name) :
       listOfReactants = get_listOfReactants(e)
       if listOfReactants== None : print("\n Warning:",reactionId, "listOfReactants=None")
       else:
-          for r in listOfReactants:
-            lpfacts.add(Term('reactant', ["\""+r.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
+        for r in listOfReactants:
+          lpfacts.add(Term('reactant', ["\""+r.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
 
       listOfProducts = get_listOfProducts(e)
       if listOfProducts== None : print("\n Warning:",reactionId, "listOfProducts=None")
       else:
-          for p in listOfProducts:
-            lpfacts.add(Term('product', ["\""+p.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
+        for p in listOfProducts:
+          lpfacts.add(Term('product', ["\""+p.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
 
   return lpfacts
 
 
 def readSBMLnetwork_with_score(filename, name) :
 
-  lpfacts = TermSet()
-
-  tree = etree.parse(filename)
-  sbml = tree.getroot()
-  model = get_model(sbml)
-
+  lpfacts         = TermSet()
+  tree            = etree.parse(filename)
+  sbml            = tree.getroot()
+  model           = get_model(sbml)
   listOfReactions = get_listOfReactions(model)
+
   for e in listOfReactions:
     if e.tag[0] == "{":
       uri, tag = e.tag[1:].split("}")
@@ -148,34 +146,33 @@ def readSBMLnetwork_with_score(filename, name) :
       listOfReactants = get_listOfReactants(e)
       if listOfReactants== None : print("\n Warning:",reactionId, "listOfReactants=None")
       else:
-          for r in listOfReactants:
-            lpfacts.add(Term('reactant', ["\""+r.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
+        for r in listOfReactants:
+          lpfacts.add(Term('reactant', ["\""+r.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
 
       listOfProducts = get_listOfProducts(e)
       if listOfProducts== None : print("\n Warning:",reactionId, "listOfProducts=None")
       else:
-          for p in listOfProducts:
-            lpfacts.add(Term('product', ["\""+p.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
+        for p in listOfProducts:
+          lpfacts.add(Term('product', ["\""+p.attrib.get("species")+"\"", "\""+reactionId+"\"","\""+name+"\""]))
 
       score = get_score(e)
       if score != 0:
-          value = int(float(score)*1000)
-          lpfacts.add(Term('value', ["\""+reactionId+"\"", str(value)]))
+        value = int(float(score)*1000)
+        lpfacts.add(Term('value', ["\""+reactionId+"\"", str(value)]))
       else :
-          #print " no value for ",Reaction_ID
-          lpfacts.add(Term('value', ["\""+reactionId+"\"", "0"]))
+        #print " no value for ",Reaction_ID
+        lpfacts.add(Term('value', ["\""+reactionId+"\"", "0"]))
 
   return lpfacts
 
 def readSBMLtargets(filename) :
 
-  lpfacts = TermSet()
-
-  tree = etree.parse(filename)
-  sbml = tree.getroot()
-  model = get_model(sbml)
-
+  lpfacts       = TermSet()
+  tree          = etree.parse(filename)
+  sbml          = tree.getroot()
+  model         = get_model(sbml)
   listOfSpecies = get_listOfSpecies(model)
+
   for e in listOfSpecies:
     if e.tag[0] == "{":
       uri, tag = e.tag[1:].split("}")
@@ -186,13 +183,12 @@ def readSBMLtargets(filename) :
 
 
 def readSBMLseeds(filename) :
-  lpfacts = TermSet()
-
-  tree = etree.parse(filename)
-  sbml = tree.getroot()
-  model = get_model(sbml)
-
+  lpfacts       = TermSet()
+  tree          = etree.parse(filename)
+  sbml          = tree.getroot()
+  model         = get_model(sbml)
   listOfSpecies = get_listOfSpecies(model)
+
   for e in listOfSpecies:
     if e.tag[0] == "{":
       uri, tag = e.tag[1:].split("}")
