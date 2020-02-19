7722c1de5b6d83fec6339259fc2051f71a7bcb24
Ariane <50831545+AriBad@users.noreply.github.com>
2020-02-19 11:25:55 +0100

    try to remove " " to results of reaction ids

--- test/test.py
+++ test/test.py
@@ -4,15 +4,15 @@ sys.path.append('../')
 from meneco import meneco
 
 def reaction_ids(prediction):
-    return set([p.arg(0) for p in prediction if p.pred() == "xreaction"])
+    return set([str(p.arg(0)).strip("\"") for p in prediction if p.pred() == "xreaction"])
 
 def test_meneco() : 
     unproducible_targets_result, reconstructable_result, one_min_sol_result, intersection_sol_result, union_sol_result, enumeration_sol_result = meneco.run_meneco('../toy/draft.sbml', '../toy/seeds.sbml', '../toy/targets.sbml', '../toy/repair.sbml', True)
     
     unproducible_targets = set(['M_cu2_c', 'M_dgtp_c', 'M_sheme_c', 'M_cl_c', 'M_2ohph_c', 'M_mlthf_c', 'M_fad_c', 'M_udcpdp_c', 'M_datp_c', 'M_ribflv_c', 'M_mobd_c', 'M_nad_c', 'M_met_DASH_L_c', 'M_gtp_c', 'M_nadp_c', 'M_dctp_c', 'M_10fthf_c', 'M_pe161_c', 'M_thf_c', 'M_pe161_p', 'M_pe160_c', 'M_adp_c', 'M_utp_c', 'M_pheme_c', 'M_cys_DASH_L_c', 'M_pydx5p_c', 'M_amet_c', 'M_fe3_c', 'M_so4_c', 'M_coa_c', 'M_pe160_p'])
-    one_min_sol = set(['"R_CU2tpp"', '"R_CLt3_2pp"'])
-    union_sol = set(['"R_CU2tpp"', '"R_CLt3_2pp"'])
-    intersection_sol = set(['"R_CU2tpp"', '"R_CLt3_2pp"'])
+    one_min_sol = set(['R_CU2tpp', 'R_CLt3_2pp'])
+    union_sol = set(['R_CU2tpp', 'R_CLt3_2pp'])
+    intersection_sol = set(['R_CU2tpp', 'R_CLt3_2pp'])
     enumeration_sol = [{'R_CLt3_2pp', 'R_CU2tpp'}, {'R_CLt3_2pp', 'R_CU2tpp'}]
 
 
