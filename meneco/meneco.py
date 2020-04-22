99d5169423397120e1a4f0aaa199b511ec2c8bc2
cfrioux <clemence.frioux@gmail.com>
2020-04-22 17:03:54 +0200

    quit without reading the repair file if no unproducible target

--- meneco/meneco.py
+++ meneco/meneco.py
@@ -115,8 +115,13 @@ def run_meneco(draftnet: str, seeds: str, targets: str, repairnet: str, enumerat
     if not json:
         print('{0} unproducible targets:\n\t{1}\n'.format(
             len(unproducible_targets_lst), '\n\t'.join(unproducible_targets_lst)))
+    
+    if len(unproducible_targets_lst) == 0:
+        utils.clean_up()
+        return result
 
     if repairnet == None:
+        utils.clean_up()
         return result
 
     logger.info('Reading repair db ...')
